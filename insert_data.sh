0c110ddeeab41fc592155bebbbd65a321260dd3c
Modou Niane <mniane2005@gmail.com>
2025-10-01 11:08:01 +0000

    ameliorer insert_data.sh

--- insert_data.sh
+++ insert_data.sh
@@ -1,24 +1,46 @@
 #!/bin/bash
-#Script to insert data from courses.csv and students.csv into students database
-cat courses.csv
+
+# Script to insert data from courses.csv and students.csv into students database
+
 PSQL="psql -X --username=freecodecamp --dbname=students --no-align --tuples-only -c"
-cat courses.csv | while IFS="," read MAJOR COURSE
+
+cat courses_test.csv | while IFS="," read MAJOR COURSE
 do
-   # get major_id
+  if [[ $MAJOR != major ]]
+  then
+    # get major_id
+    MAJOR_ID=$($PSQL "SELECT major_id FROM majors WHERE major='$MAJOR'")
 
-  # if not found
+    # if not found
+    if [[ -z $MAJOR_ID ]]
+    then
+      # insert major
+      INSERT_MAJOR_RESULT=$($PSQL "INSERT INTO majors(major) VALUES('$MAJOR')")
+      if [[ $INSERT_MAJOR_RESULT == "INSERT 0 1" ]]
+       then
+       echo Inserted into majors, $MAJOR
+      fi
+      # get new major_id
+      MAJOR_ID=$($PSQL "select major_id from majors where major='$MAJOR'")
 
-  # insert major
+    fi
 
-  # get new major_id
+    # get course_id
+     COURSE_ID=$($PSQL "select course_id from courses where course='$COURSE'")
 
-  # get course_id
+    # if not found
+      if [[ -z $COURSE_ID ]]
+       then
+    # insert course
+     INSERT_COURSE_RESULT=$($PSQL "insert into courses(course) values ('$COURSE')")
+    if [[ $INSERT_COURSE_RESULT == "INSERT 0 1" ]]
+     then
+     echo "Inserted into courses, $COURSE"
+    fi
+    # get new course_id
+      fi
 
-  # if not found
+    # insert into majors_courses
 
-  # insert course
-
-  # get new course_id
-
-  # insert into majors_courses
+  fi
 done
